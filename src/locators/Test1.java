bdd5464fa5ceecb555bcca53f1bc1e7bb9e41cbb
QSP <QSP@DESKTOP-EVK9GMC>
2021-03-18 08:25:20 +0530

    Synchronization

--- src/locators/Test1.java
+++ src/locators/Test1.java
@@ -1,8 +1,13 @@
 package locators;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Test1 {
 
@@ -10,9 +15,16 @@ public class Test1 {
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://demo.actitime.com/login.do");
 	
+driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+	
 driver.findElement(By.id("username")).sendKeys("admin");
-driver.findElement(By.name("pwd")).sendKeys("manager");	
-driver.findElement(By.xpath("//div[text()='Login ']")).click();
+driver.findElement(By.name("pwd")).sendKeys("manager");
+
+
+WebElement ele = driver.findElement(By.xpath("//div[text()='Login ']"));
+
+WebDriverWait w=new WebDriverWait(driver,5);
+w.until(ExpectedConditions.elementToBeClickable(ele)).click();
 
 String title = driver.getTitle();
 System.out.println(title);
